d07ca95ce0233ec4f2ffcecb82b32d77b20b0e25
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-02-28 15:31:33 +0800

    Updated views.py
    - created list view and detail view for merchstore products

--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Product
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product.html'
