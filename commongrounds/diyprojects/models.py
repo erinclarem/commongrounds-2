9dd2f275643744dc70804be343f3308f5d2bb848
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-02-28 22:13:58 +0800

    created models

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('diyprojects:project_detail', args=[str(self.id)])
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ['name']
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ProjectCategory,
+        on_delete=models.SET_NULL,
+        related_name='category'
+    )
+    description = models.TextField()
+    materials = models.TextField()
+    steps = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'project'
+        verbose_name_plural = 'projects'
+        ordering = ['-created_on']
