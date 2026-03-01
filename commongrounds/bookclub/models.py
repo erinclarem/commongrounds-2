cf20fde4f2d98962451d9ccef7f50837a39f3b18
Krystal <krystal.limtiongsoon@student.ateneo.edu>
2026-03-01 11:05:34 +0800

    created models
    also edited base.html to match the other apps

--- commongrounds/bookclub/models.py
+++ commongrounds/bookclub/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Genre (models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    def get_absolute_url(self):
+        return reverse('bookclub:book_detail', args=[str(self.id)])
+    
+    class Meta:
+        verbose_name = 'genre'
+        verbose_name_plural = 'genres'
+        ordering = ['name']
+
+    
+class Book (models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='genre'
+    )
+    author = models.CharField()
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
+        ordering = ['-publication_year']
+    
+
