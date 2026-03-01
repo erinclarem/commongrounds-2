ea39c07428d2d5b2b934e3116961bb6d3d307db0
fwanana <francine.denise.lee@student.ateneo.edu>
2026-03-01 15:42:22 +0800

    created base, index, localeventslist, and localeventdetail html files.

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-
+from .views import LocalEventsListView, LocalEventsDetailView
+urlpatterns = [
+    path('localevents/list', LocalEventsListView.as_view(), name='local_events_list'),
+    path('localevents/event/<int:pk>/', LocalEventsDetailView.as_view(), name='local_event_detail'),
+    ]
 
 app_name = 'localevents'
