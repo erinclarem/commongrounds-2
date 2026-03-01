df4158018c1eff34add426f571a46a7322aaaad2
ranz <edward.francis.ibera@student.ateneo.edu>
2026-03-02 04:38:16 +0800

    Created admin.py

--- commongrounds/commissions/admin.py
+++ commongrounds/commissions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CommissionType, Commission
 
-# Register your models here.
+
+class CommissionTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'people_required', 'created_on']
+
+
+admin.site.register(CommissionType, CommissionTypeAdmin)
+admin.site.register(Commission, CommissionAdmin)
