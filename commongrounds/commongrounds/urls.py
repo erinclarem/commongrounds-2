bc323bd21600bd38541ae6b986b7cce6db8fc54c
fwanana <francine.denise.lee@student.ateneo.edu>
2026-03-01 18:36:49 +0800

    restricted paths to logged-in users using LoginRequireMixin.

--- commongrounds/commongrounds/urls.py
+++ commongrounds/commongrounds/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('localevents/', include('localevents.urls', namespace='localevents')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
