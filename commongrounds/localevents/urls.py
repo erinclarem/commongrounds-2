70de470f03f58d0574fe6c1b8e680805706c8864
fwanana <francine.denise.lee@student.ateneo.edu>
2026-03-01 16:32:47 +0800

    Updated Localevent_detail.html to match decriptions added in models.py. Fixed error in urls that made localevents repeat in the path

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import LocalEventsListView, LocalEventDetailView
 urlpatterns = [
-    path('localevents/list', LocalEventsListView.as_view(), name='localevents_list'),
-    path('localevents/event/<int:pk>/',
+    path('list', LocalEventsListView.as_view(), name='localevents_list'),
+    path('event/<int:pk>/',
          LocalEventDetailView.as_view(), name='localevent_detail'),
 ]
 
