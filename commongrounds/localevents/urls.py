0fd5adcd8af14314e9ce57ea30aaa2ac0ac7e427
fwanana <francine.denise.lee@student.ateneo.edu>
2026-04-23 00:52:32 +0800

    fixed localevents url..
    from localevents/list to localevents/event (sorry!)

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import LocalEventsListView, LocalEventDetailView
 urlpatterns = [
-    path('list', LocalEventsListView.as_view(), name='localevents_list'),
+    path('events', LocalEventsListView.as_view(), name='localevents_list'),
     path('event/<int:pk>/',
          LocalEventDetailView.as_view(), name='localevent_detail'),
 ]
