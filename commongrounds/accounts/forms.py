0245954bb9f66494914979d8fdf24789fbdc2599
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-04-27 21:32:18 +0800

    added views and urls

--- commongrounds/accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Profile
-
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['display_name']
--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    display_name = models.CharField(max_length=63)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.display_name}"
