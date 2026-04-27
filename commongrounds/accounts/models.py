408f1fd28c92a1404be75fa5777c37fea2e42d41
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-04-27 22:47:51 +0800

    changed format for roles

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -10,6 +10,15 @@ class Profile(models.Model):
     )
     display_name = models.CharField(max_length=63)
     email = models.EmailField()
+    ROLE_CHOICES = [
+        ("Market Seller", "Market Seller"),
+        ("Event Organizer", "Event Organizer"),
+        ("Book Contributor", "Book Contributor"),
+        ("Project Creator", "Project Creator"),
+        ("Commission Maker", "Commission Maker"),
+    ]
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, blank=True)
 
-    def __str__(self):
-        return f"{self.display_name}"
+
+def __str__(self):
+    return f"{self.display_name}"
