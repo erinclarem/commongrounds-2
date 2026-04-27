6c7ef6d422a2f72d1ab5e632f7bce8cd54ad0ff5
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-04-27 17:24:43 +0800

    added new urls

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import ProjectListView, ProjectDetailView
+from .views import ProjectCreateView, ProjectUpdateView
 
 app_name = "diyprojects"
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('projects', ProjectListView.as_view(), name='project_list'),
     path('project/<int:pk>/', ProjectDetailView.as_view(),
          name='project_detail'),
+    path('project/add', ProjectCreateView.as_view(), name='project_add'),
+    path('project/<int:pk>/edit', ProjectUpdateView.as_view(),
+         name='project_edit'),
 ]
