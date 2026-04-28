f89c42ae541f3a75f6c986d319d069e902f19cc4
fwanana <francine.denise.lee@student.ateneo.edu>
2026-04-29 04:42:33 +0800

    Updated Event model and added eventsignupmodel

--- commongrounds/localevents/models.py
+++ commongrounds/localevents/models.py
@@ -1,5 +1,9 @@
+from cProfile import Profile
+
 from django.db import models
 from django.urls import reverse
+
+from commongrounds.commongrounds import settings
 
 
 class EventType(models.Model):
@@ -19,12 +23,30 @@ class Event(models.Model):
         EventType,
         on_delete=models.SET_NULL,
         null=True,
+        editable=False,
         related_name='events'
     )
+    organizers = models.ManyToManyFiels(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='organized_events'
+        )
+    event_image = models.ImageField(upload_to='event_images/')
     description = models.TextField()
     location = models.CharField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    event_capacity = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    status = models.CharField(
+        max_length=20, 
+        choices=(
+            ('available', 'Available'),
+            ('full', 'Full'),
+            ('done', 'Done'),
+            ('canceled', 'Canceled'),
+            ), 
+        default='available')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -37,5 +59,20 @@ class Event(models.Model):
     class Meta:
         ordering = ['-created_on']
 
+class EventSignup(models.Model):
+
+    Event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name='event_signups'
+    )
+    User_Registrant = models.ForeignKey(
+        Profile,
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='event_signups'
+    )
+     
+    New_Registrant = models.CharField()
 
 # Create your models here.
