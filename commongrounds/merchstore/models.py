f2b60c09bb0f0d34abe0df70f7cdba5853d04a05
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-04-28 18:23:29 +0800

    Updated models

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from accounts.models import Profile
+from django.core.validators import MinValueValidator
 
 
 class ProductType(models.Model):
@@ -26,8 +28,27 @@ class Product(models.Model):
         null=True,
         related_name='products'
         )
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    product_image = models.ImageField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+        )
+    stock = models.PositiveIntegerField()
+    status = models.CharField(
+        choices=[
+            ('available', 'Available'),
+            ('on sale', 'On Sale'),
+            ('out of stock', 'Out of Stock')
+            ],
+        default='available',
+        )
 
     def __str__(self):
         return f"{self.name}"
@@ -39,3 +60,28 @@ class Product(models.Model):
         ordering = ['name']
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='transactions'
+        )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='transactions' 
+        )
+    amount = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    status = models.CharField(
+        choices=[
+            ('on cart', 'On Cart'),
+            ('to pay', 'To Pay'),
+            ('to ship', 'To Ship'),
+            ('to receive', 'To Receive'),
+            ('delivered', 'Delivered')
+            ],
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
