be36e2d198d115199b9c80fa971bafbb741f7131
fwanana <francine.denise.lee@student.ateneo.edu>
2026-04-29 04:52:52 +0800

    added initial views to views.py, to be fixed at a later date

--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-from .models import Event
+from .models import Event, EventType, EventSignup
 # Create your views here.
 
 
@@ -16,3 +17,18 @@ class LocalEventsListView(ListView):
 class LocalEventDetailView(DetailView):
     model = Event
     template_name = "localevent_detail.html"
+
+class LocalEventAddView(CreateView):
+    model = Event
+    template_name = "localevent_form.html"
+    fields = ['title', 'category', 'organizers', 'event_image', 'description', 'location', 'start_time', 'end_time', 'event_capacity']
+
+class LocalEventEditView(UpdateView):
+    model = Event
+    template_name = "localevent_form.html"
+    fields = ['title', 'category', 'organizers', 'event_image', 'description', 'location', 'start_time', 'end_time', 'event_capacity']
+
+class LocalEventSignupView(CreateView):
+    model = EventSignup
+    template_name = "localevent_signup_form.html"
+    fields = ['Event', 'User_Registrant', 'New_Registrant']
