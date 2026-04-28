08e51071e50c20038d6db7be38a27fe7bedef32a
fwanana <francine.denise.lee@student.ateneo.edu>
2026-04-29 04:19:33 +0800

    Updated urls.py to include event create, update, and signup view

--- commongrounds/localevents/urls.py
+++ commongrounds/localevents/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import LocalEventsListView, LocalEventDetailView
+from .views import LocalEventsListView, LocalEventDetailView, LocalEventAddView, LocalEventEditView, LocalEventSignupView
 urlpatterns = [
     path('list', LocalEventsListView.as_view(), name='localevents_list'),
     path('event/<int:pk>/',
          LocalEventDetailView.as_view(), name='localevent_detail'),
+    path('event/add', LocalEventAddView.as_view(), name='localevent_add'),
+    path('event/<int:pk>/edit',
+         LocalEventEditView.as_view(), name='localevent_edit'),
+    path('event/<int:pk>/signup',
+         LocalEventSignupView.as_view(), name='localevent_signup'),
 ]
 
 app_name = 'localevents'
