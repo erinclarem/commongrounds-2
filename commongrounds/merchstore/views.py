c39fb13ae956f3f1ecfeba991a215bbeaebaee47
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-04-30 18:57:54 +0800

    Updated Product Detail View

--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -2,6 +2,8 @@ from .models import Product, Transaction
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
+from django.shortcuts import redirect
+from .forms import TransactionForm
 
 
 class ProductListView(ListView):
@@ -10,6 +12,7 @@ class ProductListView(ListView):
 
     def get_queryset(self):
         return super().get_queryset()
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['all_products'] = Product.objects.all()
@@ -22,6 +25,38 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['transaction_form'] = TransactionForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        transaction_form = TransactionForm(request.POST)
+
+        if not request.user.is_authenticated:
+            return redirect('accounts:login')
+
+        if transaction_form.is_valid():
+            if request.user.profile == self.object.owner:
+                transaction_form.add_error(
+                    None, 'You cannot purchase your own product.'
+                    )
+            else:
+                if transaction_form.cleaned_data['amount'] > self.object.stock:
+                    transaction_form.add_error(
+                        'amount', 'Not enough stock available.'
+                        )
+                else:
+                    transaction = transaction_form.save(commit=False)
+                    transaction.buyer = request.user.profile
+                    transaction.product = self.object
+                    transaction.save()
+                    return redirect('cart') 
+        return self.render_to_response(
+            self.get_context_data(transaction_form=transaction_form)
+            )
 
 
 class ProductCreateView(CreateView):
