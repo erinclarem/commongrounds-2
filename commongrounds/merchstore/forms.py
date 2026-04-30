884dcb717a85011a76f0cb34c7773b4fd9642fc5
erinclarem <erin.clare.maranan@student.ateneo.edu>
2026-04-30 21:18:44 +0800

    Updated form and template for Product Create View

--- commongrounds/merchstore/forms.py
+++ commongrounds/merchstore/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Transaction
+from .models import Product, Transaction
 
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ['amount']
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = ["owner"]
