aa3a79d25f11b6feffacfdcca9a83b3bf7926ff6
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-05-02 02:45:28 +0800

    Updated Project List View and Template

--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -1,13 +1,54 @@
 from .models import Project
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import RoleRequiredMixin
 
 
 class ProjectListView(ListView):
     model = Project
     template_name = "projectlist.html"
 
+    def get_queryset(self):
+        queryset = Project.objects.all()
+
+        if self.request.user.is_authenticated:
+            profile = self.request.user.profile
+            queryset = queryset.exclude(creator=profile)
+            queryset = queryset.exclude(favorite__profile=profile)
+            queryset = queryset.exclude(projectreview__reviewer=profile)
+
+        return queryset.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_projects'] = self.object_list
+
+        if self.request.user.is_authenticated:
+            profile = self.request.user.profile
+            context['created_projects'] = Project.objects.filter(
+                creator=profile
+            )
+            context['favorited_projects'] = Project.objects.filter(
+                favorite__profile=profile
+            ).distinct()
+            context['reviewed_projects'] = Project.objects.filter(
+                projectreview__reviewer=profile
+            ).distinct()
+
+        return context
+
 
 class ProjectDetailView(DetailView):
     model = Project
     template_name = "projectdetail.html"
+
+
+class ProjectCreateView(RoleRequiredMixin, CreateView):
+    required_role = "Project Creator"
+    model = Project
+    template_name = 'projectcreate.html'
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
