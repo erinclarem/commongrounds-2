7c8d2fe43046acd7881366bbfe6a9eb3eb70ae43
fwanana <francine.denise.lee@student.ateneo.edu>
2026-05-02 03:12:56 +0800

    updated views.py to make use of accounts app login mixin

--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.base import TemplateView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic.list import ListView
-from .models import Event, EventType, EventSignup
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.mixins import RoleRequiredMixin
+from .models import Event, EventSignup
+from .forms import EventForm, EventSignupForm
 
 
 class LocalEventsListView(ListView):
@@ -13,22 +10,109 @@ class LocalEventsListView(ListView):
     template_name = "localevents_list.html"
     context_object_name = 'events'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user = self.request.user
+
+        if user.is_authenticated:
+            profile = user.profile
+
+            created_events = Event.objects.filter(organizers=profile)
+            signed_events = Event.objects.filter(event_signups__user_registrant=profile)
+
+            other_events = Event.objects.all()
+            other_events = other_events.exclude(organizers=profile)
+            other_events = other_events.exclude(event_signups__user_registrant=profile)
+
+            context['created_events'] = created_events
+            context['signed_events'] = signed_events
+            context['other_events'] = other_events
+        else:
+            context['other_events'] = Event.objects.all()
+
+        return context
+
 
 class LocalEventDetailView(DetailView):
     model = Event
     template_name = "localevent_detail.html"
 
-class LocalEventAddView(CreateView):
-    model = Event
-    template_name = "localevent_form.html"
-    fields = ['title', 'category', 'organizers', 'event_image', 'description', 'location', 'start_time', 'end_time', 'event_capacity']
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-class LocalEventEditView(UpdateView):
+        event = self.object
+        user = self.request.user
+
+        can_signup = True
+
+        if user.is_authenticated:
+            profile = user.profile
+
+            # check if user is organizer
+            if event.organizers.filter(id=profile.id).exists():
+                can_signup = False
+
+        # check if event is full
+        if event.event_signups.count() >= event.event_capacity:
+            can_signup = False
+
+        context['can_signup'] = can_signup
+        return context
+
+
+class LocalEventAddView(LoginRequiredMixin, RoleRequiredMixin, CreateView):
     model = Event
     template_name = "localevent_form.html"
-    fields = ['title', 'category', 'organizers', 'event_image', 'description', 'location', 'start_time', 'end_time', 'event_capacity']
+    form_class = EventForm
+    required_role = "Event Organizer"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        profile = self.request.user.profile
+        self.object.organizers.add(profile)
+
+        return response
+
+
+class LocalEventEditView(LoginRequiredMixin, RoleRequiredMixin, UpdateView):
+    model = Event
+    template_name = "localevent_form.html"
+    form_class = EventForm
+    required_role = "Event Organizer"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        signup_count = self.object.event_signups.count()
+        capacity = self.object.event_capacity
+
+        if signup_count >= capacity:
+            self.object.status = 'Full'
+        else:
+            self.object.status = 'Available'
+
+        self.object.save()
+
+        return response
+
 
 class LocalEventSignupView(CreateView):
     model = EventSignup
     template_name = "localevent_signup_form.html"
-    fields = ['Event', 'User_Registrant', 'New_Registrant']
+    form_class = EventSignupForm
+
+    def form_valid(self, form):
+        event_id = self.kwargs['pk']
+        event = Event.objects.get(id=event_id)
+
+        form.instance.event = event
+
+        user = self.request.user
+
+        if user.is_authenticated:
+            profile = user.profile
+            form.instance.user_registrant = profile
+
+        return super().form_valid(form)
