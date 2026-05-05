4299d60ed8e7f8ab07c3907b1990145806e1d6cc
kristykrabby <kristen.caroline.reyes@student.ateneo.edu>
2026-05-05 11:55:27 +0800

    edited templates to have styling

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
