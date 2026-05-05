f365d8bd57e6666b0771053cff579c8dbe1c40d4
ranz <edward.francis.ibera@student.ateneo.edu>
2026-05-05 13:54:30 +0800

    Updated urls.py

--- commongrounds/commissions/urls.py
+++ commongrounds/commissions/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import CommissionListView, CommissionDetailView
+from .views import CommissionListView, CommissionDetailView, CommissionCreateView, CommissionUpdateView
 
 urlpatterns = [
-    path('requests', CommissionListView.as_view(), name='commission_list'),
-    path('request/<int:pk>',
-         CommissionDetailView.as_view(),
-         name='commission_detail'),
+    path('requests', CommissionListView.as_view(), name='commission-list'),
+    path('request/add', CommissionCreateView.as_view(), name='commission-create'),
+    path('request/<int:pk>', CommissionDetailView.as_view(), name='commission-detail'),
+    path('request/<int:pk>/edit', CommissionUpdateView.as_view(), name='commission-update'),
 ]
 
 app_name = 'commissions'
