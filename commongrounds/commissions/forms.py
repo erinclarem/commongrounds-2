7b2a4256fda2ca957f90b1b6813babf5ab4217a7
ranz <edward.francis.ibera@student.ateneo.edu>
2026-05-05 13:58:21 +0800

    Update forms.py

--- commongrounds/commissions/forms.py
+++ commongrounds/commissions/forms.py
@@ -1,27 +1,33 @@
 from django import forms
-from .models import Commission, Job
-
-
+from django.forms import inlineformset_factory
+ 
+from .models import Commission, Job, JobApplication
+ 
+ 
 class CommissionForm(forms.ModelForm):
     class Meta:
         model = Commission
-        exclude = ['maker']
-
-
+        fields = ['title', 'description', 'type', 'people_required', 'status']
+        widgets = {
+            'status': forms.Select(),
+        }
+ 
+ 
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ['role', 'manpower_required', 'status']
-
-
+ 
+ 
 JobFormSet = inlineformset_factory(
     Commission,
     Job,
     form=JobForm,
     extra=1,
-    can_delete=True
+    can_delete=True,
 )
-
+ 
+ 
 class JobApplicationForm(forms.ModelForm):
     class Meta:
         model = JobApplication
